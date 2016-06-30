afd5722d1430a55b0eda9cf29b4147dd02b5d606
shirne@126.com <wangyecommon1>
2016-06-30 17:55:31 +0800

    first commit

--- runtime/comps/default/zhusanjiao_manage_php/%%01^01E^01E11DF5%%bottom.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2011-11-29 17:46:22
-         compiled from public/bottom.tpl */ ?>
-<div id="footer">
-Powered By <a href="http://www.hx178.com.cn/" target="_blank">互信科技</a>	Copyright(c)2011
-</div>
-</body>
-</html>
